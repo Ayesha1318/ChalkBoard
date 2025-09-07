44d31312d72eb3b37998e5842cb0ec3b13f8635d
Ayesha Siddika <ayesha2k4as@gmail.com>
2025-09-07 21:07:33 +0530

    first

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -5,7 +5,7 @@ const userRouter = express.Router();
 const rootDir = require('../utils/pathUtil');
 
 userRouter.get("/",(req,res,next)=>{
-    res.sendFile(path.join(rootDir, 'views', 'index.html'));
+    res.sendFile(path.join(rootDir, 'views', 'home.html'));
 })
 
 
